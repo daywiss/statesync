d94d6fd6311caabea7306cb58278ed907e7d5f97
david <daywiss@gmail.com>
2016-10-20 14:56:23 -0400

    wrote state to be recursive using scope, simplifeid

--- sync-state.js
+++ sync-state.js
@@ -2,81 +2,99 @@ var assert = require('assert')
 var lodash = require('lodash')
 var Emitter = require('events')
 
-function table(value){
+module.exports = State
+
+function State(root,clone,base){
   var methods = new Emitter()
+  //holds all state
   var state = {}
+  //key for root of state, required to make lodash get and set work cleanly
+  var rootName = 'root'
+  //secondary path to construct when accessing properties on state
+  base = base || ''
+  //clone function for cloning values to and from state
+  clone = clone || function(x){return x}
+  lodash.set(state,rootName,root)
 
-  function emitChange(key,value,state){
-    methods.emit('change',key,value,lodash.cloneDeep(state))
+  function emitChange(state,path,value){
+    methods.emit('change',state,path,value)
+    methods.emit('diff',path,value)
   }
 
-  methods.init = function(value){
-    value = value || {}
-    state = lodash.cloneDeep(value) 
-    emitChange(null,value,state) 
-    return methods
-  }
-
-  methods.get = function(key){
-    if(key == null) return lodash.cloneDeep(state)
-    return lodash.cloneDeep(lodash.get(state,key))
-  }
-
-  methods.set = function(key,value){
-    value = lodash.cloneDeep(value)
-    if(key == null){
-      methods.init(value)
-      return state
+  function pathWithBase(path){
+    if(lodash.isNil(path) || lodash.isEmpty(path)){
+      return base
     }
-    lodash.set(state,key,value)
-    emitChange(key,value,state)
+    if(lodash.isArray(path)){
+      return lodash.compact(lodash.concat([base],path)).join('.')
+    }
+    if(lodash.isString(path)){
+      if(base){
+        return base + '.' + path
+      }else{
+        return path
+      }
+    }
+    assert(false,'unable to parse path, it can be an array, string or null')
+  }
+
+  function pathWithRoot(path){
+    if(lodash.isNil(path) || lodash.isEmpty(path)) return rootName
+    return rootName + '.' + path
+  }
+
+  methods.get = function(path){
+    var withBase = pathWithBase(path)
+    var withRoot = pathWithRoot(withBase)
+    return clone(lodash.get(state,withRoot))
+  }
+
+  methods.set = function(path,value){
+    var withBase = pathWithBase(path)
+    var withRoot = pathWithRoot(withBase)
+    value = clone(value)
+
+    if(lodash.isNil(value)){
+      lodash.unset(state,withRoot)
+    }else{
+      lodash.set(state,withRoot,value)
+    }
+
+    emitChange(clone(state),withBase,value)
     return value
   }
 
-  methods.delete = function(key){
-    if(key == null) state = {}
-    lodash.unset(state,key)
-    emitChange(key,null,state)
+  methods.delete = function(path){
+    var withBase = pathWithBase(path)
+    var withRoot = pathWithRoot(withBase)
+    lodash.unset(state,withRoot)
+    emitChange(clone(state),withBase)
     return true
   }
-  
-  return methods.init(value)
-}
 
-module.exports = function(){
-  var states = {}
-  var methods = new Emitter()
-
-  function changeHandler(id,key,value,state){
-    methods.emit('change',id,key,value,state)
-  }
-
-  methods.get = function(id){
-    var state = states[id]
-    assert(state,'state with this id does not exist')
-    return state
-  }
-  methods.create = function(id,value){
-    assert(states[id] == null,'this key has a state already associated with it')
-    var state = table(value)
-    states[id] = state
-    state.on('change',changeHandler.bind(null,id))
-    return state
-  }
-
-  methods.patch = function(id,key,value){
-    var state = null
-    try{
-      state = methods.get(id)
-    }catch(e){
-      state = methods.create(id)
-    }
-    if(value === null){
-      state.delete(key)
-    }else{             
-      state.set(key,value)
+  methods.patch = function(path,value){
+    var withBase = pathWithBase(path)
+    var withRoot = pathWithRoot(withBase)
+    if(lodash.isNil(value)){
+      lodash.unset(state,withRoot)
+    }else{
+      lodash.set(state,withRoot,value)
     }
   }
+
+  methods.scope = function(path){
+    var withBase = pathWithBase(path)
+    var withRoot = pathWithRoot(withBase)
+    if(lodash.get(state,withRoot) == null){
+      lodash.set(state,withRoot,{})
+    }
+    var child = State(lodash.get(state,withRoot),clone,base)
+    child.on('change',function(state,path,value){
+      emitChange(state,pathWithBase(path),value)
+    })
+    return child
+  }
+
   return methods
 }
 
--- test.js
+++ test.js
@@ -3,59 +3,74 @@ var State = require('.')
 
 test('sync-state',function(t){
   var state = null
-  var test = null
   t.test('init',function(t){
     state = State()
     t.ok(state)
     t.end()
   })
-  t.test('create state',function(t){
-    t.ok(state.create('test'))
+  t.test('set',function(t){
+    t.ok(state.set('test','test'))
+    t.ok(state.set('test1','test1'))
     t.end()
   })
   t.test('get state',function(t){
-    test = state.get('test')
-    t.ok(test)
+    var result = state.get('test')
+    t.equal('test',result)
     t.end()
   })
-  t.test('test.set',function(t){
-    var result = test.set('this.is.a.test',true)
-    t.ok(result)
-    t.end()
-  })
-  t.test('test.get',function(t){
-    var result = test.get('this.is.a.test')
-    t.ok(result)
-    t.end()
-  })
-  t.test('test.delete',function(t){
-    var result = test.delete('this.is.a.test')
-    t.ok(result)
-    t.notOk(test.get('this.is.a.test'))
-    t.end()
-  })
-  t.test('test.set null key',function(t){
-    var result = test.set(null,{test:true})
-    t.ok(result)
-    t.ok(test.get('test'))
-    t.end()
-  })
-  t.test('test.get null',function(t){
-    var result = test.get(null)
+  t.test('get null',function(t){
+    var result = state.get()
     t.ok(result)
     t.ok(result.test)
     t.end()
   })
+  t.test('delete state',function(t){
+    var result = state.delete('test')
+    t.ok(result)
+    t.notOk(state.get('test'))
+    t.ok(state.get('test1'))
+    t.end()
+  })
+  t.test('set null',function(t){
+    var result = state.set()
+    t.notOk(result)
+    t.notOk(state.get('test1'))
+    t.end()
+  })
+  t.test('scope',function(t){
+    var test = null
+    t.test('init',function(t){
+      test = state.scope('test')
+      t.ok(test)
+      t.end()
+    })
+    t.test('set/get',function(t){
+      test.set('child','child')
+      t.equal(test.get('child'),'child')
+      t.equal(state.get('test.child'),'child')
+      t.end()
+    })
+    t.test('delete',function(t){
+      test.delete('child')
+      t.notOk(test.get('child'))
+      t.notOk(state.get('test.child'))
+      t.end()
+    })
+  })
   t.test('diff/patch',function(t){
     var s1 = State()
     var s2 = State()
-    var t1 = s1.create('test')
 
-    s1.on('change',s2.patch)
-    t1.set('test',true)
-    t1.set('flop',true)
-    t1.delete('flop')
-    t.deepEqual(t1.get(),s2.get('test').get())
+    s1.on('diff',s2.patch)
+    s2.on('diff',s1.patch)
+    s1.set('s1',true)
+    s2.set('s2',true)
+    s1.set('blah',1)
+    s2.set('blah',2)
+    s1.delete('s2')
+    t.equal(s1.get('blah'),2)
+    t.deepEqual(s1.get(),s2.get())
     t.end()
   })
+  
 })
